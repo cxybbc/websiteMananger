844603ceaabb51553087dfcd5a5b9b4e55a14dd7
cxy <2969452599@qq.com>
2026-01-28 14:45:24 +0800

    1

--- src/utils/request.js
+++ src/utils/request.js
@@ -2,8 +2,8 @@ import axios from "axios";
 import ElementUI from "element-ui";
 import router from "@/router/index.js";
 const request = axios.create({
-	baseURL: "https://websitemanage.biggerlens.cn:18060",
-	//baseURL: "http://192.168.31.181:18061",
+	//baseURL: "https://websitemanage.biggerlens.cn:18060",
+	baseURL: "http://192.168.31.181:18061",
 });
 
 request.interceptors.request.use(
@@ -17,7 +17,7 @@ request.interceptors.request.use(
 	},
 	(error) => {
 		return Promise.reject(error);
-	}
+	},
 );
 
 // response 拦截器
@@ -42,7 +42,7 @@ request.interceptors.response.use(
 	},
 	(error) => {
 		return Promise.reject(error);
-	}
+	},
 );
 
 export default request;
